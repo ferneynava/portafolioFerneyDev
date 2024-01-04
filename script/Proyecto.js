22cf735fbb6a1fb792d9e2b48df65264c2c6cded
ferneynava <ferneynava@gmail.com>
2024-01-03 23:09:57 -0500

    :white_check_mark: [Add] Section y responsive
    [Add] Section y responsive

---
+++ script/Proyecto.js
@@ -0,0 +1,31 @@
+import data from "../data.json"; 
+
+const $proyectosContainer = document.querySelector(".container-Portafolio");
+
+    data.proyectos.forEach(project => {
+        $proyectosContainer.innerHTML += `
+    <div class="container-Portafolio_Tajeta">   
+            <img class="container-Portafolio_img" src="${project.img}" alt="Proyectoimg">
+            <div class="container-Portafolio_Descripcion">
+                <h3>${project.nombre}</h3>
+                <nav>
+                    <ul class="menu-tecnologias">
+                        ${project.tecnologias.map(tecnologia => `<li><img src="${tecnologia.src}" alt="${tecnologia.alt}"><p>${tecnologia.texto}</p></li>`).join("")}
+                    </ul>
+                </nav>
+                <p class="container-Portafolio_p">
+                    ${project.descripcion}
+                </p>
+            </div>
+            <div class= "container-Portafolio_Button">
+                <a href="${project.Demo}" target="_blank">
+                    Demo
+                    <img src="external-link.svg" alt="">
+                </a>
+                <a href="${project.Código}" target="_blank">
+                    Código
+                    <img src="external-link.svg" alt="">
+                </a>
+            </div>
+        </div>
+    `})
